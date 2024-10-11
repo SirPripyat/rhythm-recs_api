426954facc84ff551237a44c0fddd8b233206bcf
leonardorossi <08.leo.rossi@gmail.com>
2024-10-10 21:59:43 -0300

    :tada: init: iniciando o projeto

---
+++ src/main/java/com/leonardorossi/rhythmrecs/RhythmRecsApplication.java
@@ -0,0 +1,13 @@
+package com.leonardorossi.rhythmrecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhythmRecsApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(RhythmRecsApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/leonardorossi/rhythmrecs/RhythmRecsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardorossi.rhythmrecs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhythmRecsApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
